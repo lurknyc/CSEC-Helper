3ae2b86646a57fb1a02a36b52a7d02ff5ee37850
lurknyc <44297393+lurknyc@users.noreply.github.com>
2021-02-16 16:24:16 -0700

    Updated Auto Nmap
    Fixed bugs and wrote some code to make sure Nmap doesn't bug out.

--- nmap-auto.py
+++ nmap-auto.py
@@ -1,11 +1,42 @@
 # Brody Larson
 
-#Importing Librarys
+#Importing Libs
 import nmap
 
+#Cue The ASCII Art
+
+print("""
+
+    ___         __           _   __                    
+   /   | __  __/ /_____     / | / /___ ___  ____ _____ 
+  / /| |/ / / / __/ __ \   /  |/ / __ `__ \/ __ `/ __  /
+ / ___ / /_/ / /_/ /_/ /  / /|  / / / / / / /_/ / /_/ /
+/_/  |_\__,_/\__/\____/  /_/ |_/_/ /_/ /_/\__,_/ .___/ 
+                                              /_/      
+
+
+                    """)
 #Letting the User Enter Ip and Port Range
 IP = input("Please Enter the Machine IP:\n")
-ports = input("Please Enter Your Desired Port Range ex. (21-443)\n") 
+
+#Checking if the IP is Valid
+
+if len(IP) > 24 :
+    print("Please Enter A Valid IP!")
+    exit()
+
+if len(IP) < 4 :
+    print("Please Enter a Valid IP!")
+    exit()
+
+ports = input("Please Enter Your Desired Port Range ex. (21-443)\n")
+
+#Checking if user actually entered ports
+
+if len(ports) != 0 :
+    print("Please enter a value for ports!!!")
+    exit()
+
 
 #Starting PortScanner Object
 nmScan = nmap.PortScanner()
@@ -14,6 +45,17 @@ nmScan = nmap.PortScanner()
 nmScan.scan(IP, ports)
 
 #Printing Results
+print("""
+
+    ____                  ____      
+   / __ \___  _______  __/ / /______
+  / /_/ / _ \/ ___/ / / / / __/ ___/
+ / _, _/  __(__  ) /_/ / / /_(__  ) 
+/_/ |_|\___/____/\__,_/_/\__/____/  
+                                         
+
+
+                    """)
 for host in nmScan.all_hosts():
      print('Host : %s (%s)' % (host, nmScan[host].hostname()))
      print('State : %s' % nmScan[host].state())
